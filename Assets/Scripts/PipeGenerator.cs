8736a16d226f7f8679e97ca94401e4e939cd018d
Igor Sokol <isaexecuter@gmail.com>
2022-05-29 20:51:10 +0300

    Add PipeSpawner

---
+++ Assets/Scripts/PipeGenerator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGenerator : ObjectPool
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private float _secondsBetweenSpawn;
+    [SerializeField] private float _maxSpawnPositionY;
+    [SerializeField] private float _minSpawnPositionY;
+
+    private float _elapsedTime = 0;
+
+    private void Start()
+    {
+        Init(_template);
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime > _secondsBetweenSpawn)
+        {
+            if (TryGetObject(out GameObject pipe))
+            {
+                _elapsedTime = 0;
+
+                float spawnPositionY = Random.Range(_minSpawnPositionY, _maxSpawnPositionY);
+                Vector3 spawnPoint = new Vector3(transform.position.x, spawnPositionY, transform.position.z);
+
+                pipe.transform.position = spawnPoint;
+                pipe.SetActive(true);
+
+                DisableObjectAbroadScren();
+            }
+        }
+    }
+}
